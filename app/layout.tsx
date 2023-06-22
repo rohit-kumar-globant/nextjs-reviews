182ee6a50fbb2f7de7ac4e5e85c7526cb3cca5cf
rohit-kumar-globant <108795419+rohit-kumar-globant@users.noreply.github.com>
2023-06-22 13:54:31 +0530

    Deployed on vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
                     <a href="https://rawg.io/"
                         className="text-orange-800 hover:underline"
                         target="_blank">RAWG</a>
+                    {' '} | Deployed on vercel
                 </footer>
             </body>
         </html>
